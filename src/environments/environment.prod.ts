5a5b9a9855424dee264de11b46b52b6fb7a557cd
nahropro <nahropro@gmail.com>
2020-12-06 23:03:37 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: true,
   firebase:{
-    
+    apiKey: "AIzaSyDp79n7pDL8nbjIevk0Z7l1mp6qdANWCYg",
+    authDomain: "chia-project.firebaseapp.com",
+    projectId: "chia-project",
+    storageBucket: "chia-project.appspot.com",
+    messagingSenderId: "452737745916",
+    appId: "1:452737745916:web:101405a1f3430d5d686879",
+    measurementId: "G-96K3B33X3W"
   }
 };
