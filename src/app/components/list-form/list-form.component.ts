40e4ff812ad7fb6d66a055ca51b4a47674c75609
nahropro <nahropro@gmail.com>
2020-12-12 14:50:03 +0300

    update

--- src/app/components/list-form/list-form.component.ts
+++ src/app/components/list-form/list-form.component.ts
@@ -1,6 +1,6 @@
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { FormServiceService } from './../../services/form-service.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Form } from 'src/app/models/form.model';
 import { DialogData } from 'src/app/models/dialog-data.model';
 import { YesNoDialogComponent } from '../yes-no-dialog/yes-no-dialog.component';
@@ -12,15 +12,21 @@ import { Clipboard } from "@angular/cdk/clipboard"
   templateUrl: './list-form.component.html',
   styleUrls: ['./list-form.component.scss']
 })
-export class ListFormComponent implements OnInit {
-  forms$: Observable<Form[]>;
+export class ListFormComponent implements OnInit, OnDestroy {
+  formSubscription: Subscription;
+  forms: Form[];
+  filterForms: Form[];
+  filterText: string='';
 
   constructor(private formService: FormServiceService,
               private dialog: MatDialog,
               private clipboard: Clipboard) { }
 
   ngOnInit(): void {
-    this.forms$ = this.formService.getAll();
+    this.formSubscription = this.formService.getAll().subscribe(f=> {
+      this.forms=f;
+      this.filter();
+    });
   }
 
   delete(id: string){
@@ -42,5 +48,20 @@ export class ListFormComponent implements OnInit {
 
   copyLink(id: string){
     this.clipboard.copy(window.location.host + '/form/fill/' + id);
+  }
+
+  filter(){
+    const pureFilterText=this.filterText.trim();
+
+    if(pureFilterText){
+      this.filterForms=this.forms.filter(d=> d.personalInfo.fullName.includes(pureFilterText));
+    }
+    else {
+      this.filterForms=this.forms;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.formSubscription.unsubscribe();
   }
 }
