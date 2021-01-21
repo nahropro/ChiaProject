2f706d005871771e968d4f305ea2cb0b3e8ce74c
nahropro <nahropro@gmail.com>
2021-01-21 11:00:17 +0300

    update

--- src/app/meta-data/form.meta-data.ts
+++ src/app/meta-data/form.meta-data.ts
@@ -7,70 +7,70 @@ export const questionGroup1: QuestionGroup = {
         {
             id: 1,
             title: 'زۆر حەزكردنی خەڵك لە فووتساڵ',
-            titlef: '',
+            titlef: 'علاقمندی زیاد مردم به فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 2,
             title: 'بەدەمەوەهاتنی تازەلاو و گەنجە بەهرەدارەكان',
-            titlef: '',
+            titlef: 'حضور نوجوانان و جوانان مستعد',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 3,
             title: 'دەست پێگەیشتنی ئاسانی هەموان بە كەش و شوێنە تایبەتییەكانی وەرزشی فووتساڵ',
-            titlef: '',
+            titlef: 'دسترسی آسان و همگان به فضاها و اماکن مختص ورزش فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 4,
             title: 'حەزی ناوەكی (زاتی) گەنجان و تازەگەنجان بۆ فووتساڵ',
-            titlef: '',
+            titlef: 'استعداد بالقوه جوانان و نوجوانان در فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 5,
             title: 'ئامادەبوونی فراوانی تیپەكان و یاریگاكانی فووتساڵ',
-            titlef: '',
+            titlef: 'حضور  گسترده تیم ها و باشگاه های فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 6,
             title: 'بوونی پسپۆڕ و ڕاهێنەری بەتوانای فووتساڵ',
-            titlef: '',
+            titlef: 'وجود متخصصین و مربیان شاخص در فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 7,
             title: 'لێهاتوویی پسپۆڕی و فەننی بەرزی ڕاهێنەرانی فووتساڵ',
-            titlef: '',
+            titlef: 'توانمندی تخصصی و فنی بالای مربیان فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 8,
             title: 'بەشداربوونی تیپی فووتساڵ لە پێشبڕكێی فەرمیی وڵاتدا',
-            titlef: '',
+            titlef: 'حضور تیم فوتسال در مسابقات رسمی کشوری',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 9,
             title: 'بەڕێوەچوونی ئاهەنگ و پێشبڕكێی فووتساڵ',
-            titlef: '',
+            titlef: 'برگزاری جشنواره ها و مسابقات متعدد فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 10,
             title: 'بوونی ناوبژیوانان و بەڕێوبەرانی بەتوانا لە فووتساڵدا',
-            titlef: '',
+            titlef: 'وجود داوران و مدیران توانمند در فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         }
@@ -85,70 +85,70 @@ export const questionGroup2: QuestionGroup = {
         {
             id: 1,
             title: 'نەبوونی بەرنامەی داڕێژراو بۆ گەشەكردنی فووتساڵ',
-            titlef: '',
+            titlef: 'عدم وجود برنامه ریزی جهت توسعه فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 2,
             title: 'هەبوونی بەڕێوبەرانی بێ توانا لە بەڕیوەبردنی فووتساڵ',
-            titlef: '',
+            titlef: 'وجود مدیران ناتوان در راس فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 3,
             title: 'كوالێتی نەگونجاوی ئامراز و كەرەستەكانی فووتساڵ',
-            titlef: '',
+            titlef: 'کیفیت نامناسب وسایل و تجهیزات فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 4,
             title: 'حسێب بۆ نەكردنی فووتساڵ',
-            titlef: '',
+            titlef: 'عدم وجود اعتبار کافی برای فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 5,
             title: 'نەبوونی سپۆنسەر و وەبەرهێن لە فووتساڵدا',
-            titlef: '',
+            titlef: 'عدم وجود اسپانسر و سرمایه گذار در فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 6,
             title: 'سوودنەبینینی پێوست لە پسپۆڕانی هەبووی فووتساڵ',
-            titlef: '',
+            titlef: 'استفاده ناکافی از کارشناسان موجود فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 7,
             title: 'نەبوونی سیستەمی بەهرە دۆزینەوە لە فووتساڵدا',
-            titlef: '',
+            titlef: 'فقدان نظام استعدادیابی در فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 8,
             title: 'چاودێری و كۆنتڕۆڵ',
-            titlef: '',
+            titlef: 'نظارت و کنترل',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 9,
             title: 'ڕێكخستنی نەگونجاوی هێز و تواناكان',
-            titlef: '',
+            titlef: 'سازماندهی نامناسب نیروها',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 10,
             title: 'حاڵی خراپی ماددیی وەرزشكاران',
-            titlef: '',
+            titlef: 'وضعیت مالی نامناسب ورزشکاران',
             normalAnswer: '',
             wantedAnswer: ''
         }
@@ -163,70 +163,70 @@ export const questionGroup3: QuestionGroup = {
         {
             id: 1,
             title: 'زیاتربوونی میدیاكانی شارەكە',
-            titlef: '',
+            titlef: 'تعدد رسانه های استان',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 2,
             title: 'گەشەسەندن و ڕوو لە زیاد بوونی زانیاری و پەیوەندییەكان',
-            titlef: '',
+            titlef: 'رشد روز افزون فن آوری اطلاعات و ارتباطات',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 3,
             title: 'هەبوونی یاسا و ڕێسای پشتگیری لە فووتساڵ',
-            titlef: '',
+            titlef: 'وجود قوانین و مقررات حمایتی از فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 4,
             title: 'حەزكردنی بەڕێوبەرە پلە بەرزەكان لە وەرزش',
-            titlef: '',
+            titlef: 'علاقمندی مدیران رده بالا به ورزش',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 5,
             title: 'پشتگیری خێزانەكان لە وەرزشكاران',
-            titlef: '',
+            titlef: 'حمایت خانواده ها از  ورزشکاران',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 6,
             title: 'خۆشویستراوی گشتی و گرنگیدانی كۆمەڵگا بە پاڵەوانان',
-            titlef: '',
+            titlef: 'محبوبیت عمومی و توجه جامعه به قهرمانان',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 7,
             title: 'کەمبوونەوەی ڕێگری ناوەندی لە فووتساڵدا',
-            titlef: '',
+            titlef: 'کاهش تصدی گری دولت مرکزی در فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 8,
             title: 'مەرزی هاوبەشی زەمینی لەگەڵ ئێراندا بۆ هاتوچۆی وەرزشی',
-            titlef: '',
+            titlef: 'مرز مشترک زمینی با ایران جهت مراودات ورزشی',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 9,
             title: 'جەختكردنەوەی ڕێنماییە ئایینییەكان لەسەر تەندوروستیی تاك',
-            titlef: '',
+            titlef: 'تاکید آموزه های دینی به سلامت افراد',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 10,
             title: 'هەبوونی دەمارگیری نەتەوەیی بۆ پشتگیری كردن لە پاڵەوانان',
-            titlef: '',
+            titlef: 'وجود تعصبات قومی جهت حمایت از قهرمانان',
             normalAnswer: '',
             wantedAnswer: ''
         }
@@ -242,70 +242,70 @@ export const questionGroup4: QuestionGroup = {
         {
             id: 1,
             title: 'بەرزبوونەوەی ئاماری بێكاری',
-            titlef: '',
+            titlef: 'افزایش آمار نرخ بیکاری',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 2,
             title: 'بەرزبوونەوەی نەخۆشی گشتگری كۆرۆنا',
-            titlef: '',
+            titlef: 'افزایش بیماری فراگیر کرونا',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 3,
             title: 'حەز بە ژیانی ڕۆبۆتی و ئامێری و ناچالای كۆمەڵگا',
-            titlef: '',
+            titlef: 'گرایش به زندگی ماشینی و غیر فعال جامعه',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 4,
             title: 'سیاسی بوونی وەرزش بە تایبەت فووتساڵ',
-            titlef: '',
+            titlef: 'سیاسی شدن ورزش بویژه فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 5,
             title: 'نەگونجانی كەش و ئاو و هەوای شوێنەكە',
-            titlef: '',
+            titlef: 'موقعیت نامناسب آب و هوایی',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 6,
             title: 'بەرزبوونەوەی نرخی كەرەستە وەرزشییەكان',
-            titlef: '',
+            titlef: 'افزایش قیمت تجهیزات ورزشی',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 7,
             title: 'دیاردەی كۆچكردنی قارەمانانی وەرزشی فووتساڵ',
-            titlef: '',
+            titlef: 'افزایش مهاجرت قهرمانان ورزشی فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 8,
             title: 'بەكارهێنانی شوێنە وەرزشییەكان بۆ شتی تر',
-            titlef: '',
+            titlef: 'استفاده غیر ورزشی از اماکن ورزشی',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 9,
             title: 'بەرزبوونەوەی نرخەكانی پاراستن و چاودێری كردنی یاریگاكانی وەرزشی فووتساڵ',
-            titlef: '',
+            titlef: 'افزایش هزینه های حفظ و نگهداری سالن های ورزشی فوتسال',
             normalAnswer: '',
             wantedAnswer: ''
         },
         {
             id: 10,
             title: 'بەرزبوونەوەی نرخەكانی فێنككەرەوە و گەرمكەرەوەی یاریگاكان',
-            titlef: '',
+            titlef: 'افزایش هزینه های سرمایشی و گرمایشی سالن ها',
             normalAnswer: '',
             wantedAnswer: ''
         }
